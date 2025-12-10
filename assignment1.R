4640bab7668ce0676d84b3f49ac0639dcc1b61ee
Yuvalb94 <yuvalbonneh@gmail.com>
2025-12-10 11:50:19 +0200

    Upgrade code in assignment 1

--- assignment1.R
+++ assignment1.R
@@ -5,10 +5,16 @@
 #### ASSIGNMENT 1 ####
 
 # 1. Generate objects:
-subject_id = c(1, 2, 3, 4, 5, 6)
-sex = c('m', 'f', 'm', 'f', 'm', 'f')
-age = c(20, 25, 21, 33, 30, 19)
-depression = c(0, 0, 1, 1, 1, 0)
+# subject_id = c(1, 2, 3, 4, 5, 6)
+# sex = c('m', 'f', 'm', 'f', 'm', 'f')
+# age = c(20, 25, 21, 33, 30, 19)
+# depression = c(0, 0, 1, 1, 1, 0)
+
+# Changed code according to what we learned
+subject_id = seq(1, 6)
+sex = sample(c('m', 'f'), 6, replace=TRUE)
+age = sample(18:35, 6, replace=TRUE)
+depression = sample(c(0, 1), 6, replace=TRUE)
 
 # 2. Concatenate to dataframe:
 df = data.frame(subject_id, sex, age, depression)
