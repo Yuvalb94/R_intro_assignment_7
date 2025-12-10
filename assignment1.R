b22863e1682869ecaf56dca38bd49d165523dca7
Yuvalb94 <yuvalbonneh@gmail.com>
2025-12-10 11:47:50 +0200

    Add assignment 1 code to repo

---
+++ assignment1.R
@@ -0,0 +1,18 @@
+# R course for beginners
+# Week 1
+# assignment by Yuval Bonneh
+
+#### ASSIGNMENT 1 ####
+
+# 1. Generate objects:
+subject_id = c(1, 2, 3, 4, 5, 6)
+sex = c('m', 'f', 'm', 'f', 'm', 'f')
+age = c(20, 25, 21, 33, 30, 19)
+depression = c(0, 0, 1, 1, 1, 0)
+
+# 2. Concatenate to dataframe:
+df = data.frame(subject_id, sex, age, depression)
+print(df)
+
+# 3. Save to .csv
+write.csv(df, "my_data.csv", row.names = FALSE)
